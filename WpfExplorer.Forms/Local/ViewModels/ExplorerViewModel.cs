37f31277500770c315287b393bb3fc410a5cb331
wooyonghyeon <singo112ok@gmail.com>
2025-10-04 21:47:30 +0900

    5.8까지함 viewmodel 두둥등장

---
+++ WpfExplorer.Forms/Local/ViewModels/ExplorerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Jamesnet.Wpf.Mvvm;
+
+namespace WpfExplorer.Forms.Local.ViewModels
+{
+    class ExplorerViewModel : ObservableBase
+    {
+        public string TestTitle { get; init; }
+
+        public ExplorerViewModel()
+        {
+            TestTitle = "WPF INSIDE OUT";
+        }
+
+    }
+}
