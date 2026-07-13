f08be9819fc97ba04da56da2e21eb9581a7b8c3b
wyh <wyh@okpos.co.kr>
2026-07-13 15:26:06 +0900

    6.15 완료

--- WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
+++ WpfExplorer.Main/Local/ViewModels/MainContentViewModel.cs
@@ -1,5 +1,8 @@
-﻿using Jamesnet.Wpf.Mvvm;
+﻿using CommunityToolkit.Mvvm.Input;
+using Jamesnet.Wpf.Mvvm;
 using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
 using WpfExplorer.Support.Local.Helpers;
 using WpfExplorer.Support.Local.Models;
 
@@ -8,11 +11,19 @@ namespace WpfExplorer.Main.Local.ViewModels
 {
     public class MainContentViewModel
     {
+        public ICommand FolderChangedCommand { get; init; }
+
         public List<FolderInfo> Roots { get; init; }
 
-        public MainContentViewModel(FileService fileServie)
+        public MainContentViewModel(FileService _fileServie)
         {
-            Roots = fileServie.GenerateRootNodes();
+            FolderChangedCommand = new RelayCommand<FolderInfo>(FolderChanged);
+            Roots = _fileServie.GenerateRootNodes();
+        }
+
+        private void FolderChanged(FolderInfo folderInfo)
+        {
+            MessageBox.Show($"Selected : {folderInfo.Name}");
         }
     }
 }
